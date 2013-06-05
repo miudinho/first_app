0e5d498bc0e7bcafde3ecf65e0a09723da82f17f
sml <sml@sml-VirtualBox.(none)>
2013-06-05 21:09:42 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppName::Application.config.secret_token = '7e7f06c7e750c17436ff1cd89b710c8ebdfec919d3ae539c0d6c0c7533441628560a66b35709d24facf2fc089bf8847bb7f523a861d7b77da5cf6487c3878195'
